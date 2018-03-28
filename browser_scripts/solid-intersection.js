e637bd9695d609c260404464dab3c002e76c0512
Lukas Vanhoucke <vanhouckelukas@gmail.com>
2018-03-28 11:23:00 +0200

    Implemented solid-intersection.js although it is not really an intersection yet. WIP.

--- browser_scripts/solid-intersection.js
+++ browser_scripts/solid-intersection.js
@@ -19,7 +19,6 @@ vocab.example = ns.base('http://www.example.com/ns#'); // TODO: Remove this by f
 
 const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
-
 // Collect annotation directory
 // Collect all annotations in this directory
 // Store the annotations in a graph
@@ -28,7 +27,6 @@ const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 
 function doIntersection() {
   return new Promise(function(resolve, reject) {
-    let annotations = [];
     solid.login() // the browser automatically provides the client key/cert for you
       .then(webId => {
         console.log('Current WebID: %s', webId);
@@ -59,39 +57,70 @@ function collectAnnotationLocations() {
   return new Promise(function(resolve, reject) {
     // Get all annotation locations
     solid.web.get(save_location)
-                  .then(function (container) {
-                    console.log(container)
-                    resolve(Object.keys(container.resource.resources));
+      .then(function (container) {
+        resolve(Object.keys(container.resource.resources));
     })
   });
 }
 
 function collectAnnotations(locations) {
   return new Promise(function(resolve, reject) {
-    let graph = rdf.graph();
+    let graph = null;
     // Collect the annotations and store in graph
     console.log(locations);
     let counter = 0;
     for (let i = 0; i < locations.length; i++) {
       let location = locations[i];
+      console.log(location);
       solid.web.get(location)
-                    .then(function (response) {
-                      console.log(response.parsedGraph());
-                      // Merge graph with current graph
-                      // graph.add(response.parsedGraph()); ??
-                      // If last location: Return the graph
-                      counter++;
-                      if (counter == locations.length) resolve(graph);
-      });
+        .then(function (response) {
+          // Merge graph with current graph
+          // console.log(response.parsedGraph());
+          if (graph === null) graph = response.parsedGraph();
+          else graph.add(response.parsedGraph());
+          // console.log(graph);
+          // If last location: Return the graph
+          counter++;
+          if (counter == locations.length) resolve(graph);
+        }).catch(function(err) {
+          reject(err);
+        });
     }
-
   });
 }
 
 function queryAnnotations(graph) {
   return new Promise(function(resolve, reject) {
+    if (graph === null) {
+      displayResults(null);
+      resolve();
+    }
+    let stringToBeContained = document.getElementById("querystring").value;
+    let results = [];
+    // example query: Find all strings that contain 'Lorem ipsum dolor'
+    console.log(graph);
+    let comments = graph.statementsMatching(undefined, vocab.rdf('value'), undefined);
+    comments.forEach(function(comment) {
+      let value = comment.object.value;
+      console.log(value);
+      if (value.indexOf(stringToBeContained) !== -1) results.push(comment);
+    })
+    displayResults(results);
     resolve();
   });
+}
+
+function displayResults(results) {
+  let resultString = "";
+  if (results === null || results.length == 0) {
+    resultString = "No results found.";
+  } else {
+    results.forEach(function(result) {
+      resultString += result.why.value + ' --> ' + result.object.value + '\n';
+    });
+  }
+
+  document.getElementById("results").value = resultString;
 }
 
 var initializeButton = document.getElementById("initializeButton");
